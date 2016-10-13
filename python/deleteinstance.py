623ccb0a51102aa0075262faddeb19b1e4af3dbd
Andrew Boles <drew.boles88@gmail.com>
2016-10-13 20:14:49 +0000

    changed nova client to v2

--- python/deleteinstance.py
+++ python/deleteinstance.py
@@ -1,13 +1,14 @@
-import novaclient.v1_1.client as nvclient
-from credentials import get_nova_creds
+#import novaclient.v1_1.client as nvclient
+from novaclient.client import Client
+from credentials import get_nova_creds_v2
 import sys
 
 # retrieve server name from command line input
 name = sys.argv[1]
 
 # grab the nova credentials from credentials.py
-creds = get_nova_creds()
-nova = nvclient.Client(**creds)
+creds = get_nova_creds_v2()
+nova = Client(**creds)
  
 server = nova.servers.find(name=name)
 server.delete()
