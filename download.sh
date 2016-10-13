1ff8a52e612fd72751d3e99bb1c999c866778fd7
Andrew Boles <drew.boles88@gmail.com>
2016-10-13 13:00:53 -0500

    Delete download.sh

--- download.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-source /home/cc/openstack_docs/CH-818165-openrc.sh
-echo `swift download Genomics/to_be_processed $input1`
-echo `swift download Genomics/to_be_processed $input2`
-echo `swift upload Genomics/currently_processing $input1`
-echo `swift upload Genomics/currently_processing $input2`
-echo `swift delete Genomics/to_be_processed $input1`
-echo `swift delete Genomics/to_be_processed $input2`
