f1b1d461874c317e41f9f4de110b699b64bc4919
Andrew Boles <drew.boles88@gmail.com>
2016-10-13 21:01:44 +0000

    Python file to output all files in a user input container.

---
+++ python/swiftlist.py
@@ -0,0 +1,26 @@
+import logging
+
+from swiftclient.service import SwiftService, SwiftError
+from sys import argv
+
+logging.basicConfig(level=logging.ERROR)
+logging.getLogger("requests").setLevel(logging.CRITICAL)
+logging.getLogger("swiftclient").setLevel(logging.CRITICAL)
+logger = logging.getLogger(__name__)
+
+container = argv[1]
+with SwiftService() as swift:
+    	try:
+        	list_parts_gen = swift.list(container=container)
+        	for page in list_parts_gen:
+            		if page["success"]:
+                		for item in page["listing"]:
+					i_size = int(item["bytes"])
+                      			i_name = item["name"]
+                        		i_etag = item["hash"]
+                        		print("%s [size: %s] [etag: %s]" %(i_name, i_size, i_etag))
+            		else:
+                		raise page["error"]
+
+    	except SwiftError as e:
+        	logger.error(e.value)
