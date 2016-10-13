9dd9dd837a79acc4226e695346700af0a79dd1f3
Chameleon Cloud User <cc@akb-2.novalocal>
2016-10-13 18:10:01 +0000

    Add existing file

--- bash/download.sh
+++ bash/download.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-source /home/cc/openstack_docs/CH-818165-openrc.sh
-echo `swift download Genomics/to_be_processed $input1`
-echo `swift download Genomics/to_be_processed $input2`
-echo `swift upload Genomics/currently_processing $input1`
-echo `swift upload Genomics/currently_processing $input2`
-echo `swift delete Genomics/to_be_processed $input1`
-echo `swift delete Genomics/to_be_processed $input2`
+
+echo "starting"
+#echo `./createopenrc.sh`
+source /tmp/openrc
+echo `swift download test_restore test.txt`
