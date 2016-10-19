a7a2b96068a72782ceaa529520d13f2639141813
Chameleon Cloud User <cc@akb-1.novalocal>
2016-10-19 14:03:43 +0000

    changed to be a function that can be called from another script

--- python/checkswift.py
+++ python/checkswift.py
@@ -1,36 +1,27 @@
-import logging
-
+#!/usr/bin/env python
 from swiftclient.service import SwiftService, SwiftError
 from sys import argv
 
-# logging information
-logging.basicConfig(level=logging.ERROR)
-logging.getLogger("requests").setLevel(logging.CRITICAL)
-logging.getLogger("swiftclient").setLevel(logging.CRITICAL)
-logger = logging.getLogger(__name__)
-
 # define function to check for certain file type
-def is_filetype(x):
-    return (
-        x["name"].lower().endswith(argv[3])
-    )
+def is_filetype(x, filetype):
+    	return (
+        	x["name"].lower().endswith(filetype)
+    	)
 
-# first argument is container name, second argument is folder name
-container = argv[1]
-list_options = {"prefix": argv[2]}
-with SwiftService() as swift:
-    	try:
-        	list_parts_gen = swift.list(container=container, options=list_options)
-        	for page in list_parts_gen:
-            		if page["success"]:
-                		for item in page["listing"]:
-					if is_filetype(item):	
-						i_size = int(item["bytes"])
-                      				i_name = item["name"]
-                        			i_etag = item["hash"]
-                        			print("%s [size: %s] [etag: %s]" %(i_name, i_size, i_etag))
-            		else:
-                		raise page["error"]
+def file_present(container, folder, filetype):
+	# first argument is container name, second argument is folder name
+	list_options = {"prefix": folder}
+	with SwiftService() as swift:
+    		try:
+        		list_parts_gen = swift.list(container=container, options=list_options)
+        		for page in list_parts_gen:
+            			if page["success"]:
+                			for item in page["listing"]:
+						if is_filetype(item, filetype):	
+							return True
+					return False
+            			else:
+                			raise page["error"]
 
-    	except SwiftError as e:
-        	logger.error(e.value)
+    		except SwiftError as e:
+			print('Error: %s' %e)
