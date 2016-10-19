13264efa0eb2607ea68fa75dfde3e9f9a5296d98
Chameleon Cloud User <cc@akb-1.novalocal>
2016-10-19 14:03:50 +0000

    changed to be a function that can be called from another script

--- python/downloadfile.py
+++ python/downloadfile.py
@@ -1,32 +1,26 @@
-import logging
-
+#!/usr/bin/env python
 from swiftclient.service import SwiftService, SwiftError
-from sys import argv
 
-logging.basicConfig(level=logging.ERROR)
-logging.getLogger("requests").setLevel(logging.CRITICAL)
-logging.getLogger("swiftclient").setLevel(logging.CRITICAL)
-logger = logging.getLogger(__name__)
-
-container = argv[1]
-with SwiftService() as swift:
-    try:
-        list_options = {"prefix": argv[2]}
-        list_parts_gen = swift.list(container=container, options=list_options)
-        for page in list_parts_gen:
-            if page["success"]:
-                objects = [
-                    obj["name"] for obj in page["listing"]
-                ]
-		print(objects)
-                for down_res in swift.download(
-                        container=container,
-                        objects=objects):
-                    if down_res['success']:
-                        print("'%s' downloaded" % down_res['object'])
-                    else:
-                        print("'%s' download failed" % down_res['object'])
-            else:
-                raise page["error"]
-    except SwiftError as e:
-        logger.error(e.value)
+def download_files(container, folder):
+	# argument one is the container name
+	with SwiftService() as swift:
+    		try:
+			# argument two is the folder name within the container
+        		list_options = {"prefix": folder}
+        		list_parts_gen = swift.list(container=container, options=list_options)
+        		for page in list_parts_gen:
+            			if page["success"]:
+                			objects = [
+                    				obj["name"] for obj in page["listing"]
+                			]
+                			for down_res in swift.download(container=container,objects=objects):
+                    				if down_res['success']:
+                        				print("'%s' downloaded" % down_res['object'])
+                    				else:
+                        				print("'%s' download failed" % down_res['object'])
+						print('stuck here?')
+            			else:
+                			raise page["error"]
+    		except SwiftError as e:
+			print('Error: %s' %e)
+	return
