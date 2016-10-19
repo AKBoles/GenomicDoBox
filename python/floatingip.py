08a9bd6be81597d3b8a1f187fcc4ebbdd56716b7
Chameleon Cloud User <cc@akb-1.novalocal>
2016-10-19 00:49:35 +0000

    now can use floatingip.py to create and allocate a floating ip to user-input instance

--- python/floatingip.py
+++ python/floatingip.py
@@ -3,8 +3,14 @@
 from credentials import get_nova_creds_v2
 from novaclient.client import Client
 
+# get nova credentials
 credentials = get_nova_creds_v2()
 nova_client = Client(**credentials)
-ip_list = nova_client.floating_ips.list()
-for item in ip_list:
-	print(item)
+
+# create a new floating ip from the addresses available
+ip_list = nova_client.floating_ip_pools.list()
+floating_ip = nova_client.floating_ips.create(ip_list[0].name)
+
+# assign the created ip address to the instance input by user
+instance = nova_client.servers.find(name=sys.argv[1])
+instance.add_floating_ip(floating_ip)
