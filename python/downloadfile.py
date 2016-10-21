fcac351e11bb5adba5588aa1a80965a51385af05
Chameleon Cloud User <cc@akb-1.novalocal>
2016-10-21 21:35:56 +0000

    added delete file boolean

--- python/downloadfile.py
+++ python/downloadfile.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
 from swiftclient.service import SwiftService, SwiftError
+from deletefile import delete_files
 
-def download_files(container, folder):
+# add in a delete flag that will delete the file after if input true
+def download_files(container, folder, delete_flag):
 	# argument one is the container name
+	# argument two is the folder name within the container
+	# argument three is a flag to delete file from container if set true
 	with SwiftService() as swift:
     		try:
-			# argument two is the folder name within the container
         		list_options = {"prefix": folder}
         		list_parts_gen = swift.list(container=container, options=list_options)
         		for page in list_parts_gen:
@@ -14,9 +17,13 @@ def download_files(container, folder):
 					for result in swift.download(container=container, objects=objects):
 						if result["success"]:
 							print("Downloaded %s successfully." %result["object"])
+							if delete_flag:
+								delete_files(container, objects)				
 						else:
 							print("Failed to download %s." %result["object"])
             			else:
                 			raise page["error"]
     		except SwiftError as e:
 			print('Error: %s' %e)
+
+download_files("test_restore", "testing/blah", True)
