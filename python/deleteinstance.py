96352a2b3cef219538bcd28665c9da7046423a22
Chameleon Cloud User <cc@akb-1.novalocal>
2016-10-21 22:36:40 +0000

    changed it back to a individual script

--- python/deleteinstance.py
+++ python/deleteinstance.py
@@ -1,24 +1,25 @@
 #!/usr/bin/env python
 from novaclient.client import Client
 from credentials import get_nova_creds_v2
-import sys
+from sys import argv
 
-def delete_server(name):
-	# grab the nova credentials from credentials.py
-	creds = get_nova_creds_v2()
-	nova = Client(**creds)
+# first argument is the server name
+name = argv[1]
+# grab the nova credentials from credentials.py
+creds = get_nova_creds_v2()
+nova = Client(**creds)
  
-	servers_list = nova.servers.list()
-	server_exists = False
+servers_list = nova.servers.list()
+server_exists = False
 
-	for s in servers_list:
-		if s.name == name:
-			server_exists = True
-			break
-	if not server_exists:
-		print("The server %s does not exist." % name)
-	else:
-		print("Deleting server: %s" % name)
-		server = nova.servers.find(name=name)
-		server.delete()
-		print("The server %s was deleted." % name)
+for s in servers_list:
+	if s.name == name:
+		server_exists = True
+		break
+if not server_exists:
+	print("The server %s does not exist." % name)
+else:
+	print("Deleting server: %s" % name)
+	server = nova.servers.find(name=name)
+	server.delete()
+	print("The server %s was deleted." % name)
