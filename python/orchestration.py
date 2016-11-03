5f749f40fc51f331c50724c230535c81bbf8b52a
Chameleon Cloud User <cc@akb-dobox.novalocal>
2016-11-03 06:16:04 +0000

    added first logfile.txt implementation

--- python/orchestration.py
+++ python/orchestration.py
@@ -33,12 +33,12 @@ def chdir(dest):
 	new = os.getcwd()
 	print('old: %s, new: %s' %(old, new))
 
-def prepref():
+def prepref(logfile):
 	# prepare reference genome for use by calling prepareref.json
-	if runp.runjsoncommands(JSON_DIRECTORY + 'prepareref.json'):
+	if runp.runjsoncommands(JSON_DIRECTORY + 'prepareref.json', logfile):
         	print('Preparing Reference Genome was successful.')
 
-def checkref():
+def checkref(logfile):
 	# first check for hg19 database:
 	if not os.listdir(HG19_DIRECTORY):
 		chdir(HG19_DIRECTORY)
@@ -53,12 +53,13 @@ def checkref():
 	elif len(os.listdir(REF_DIRECTORY)) is 1:
 		chdir(REF_DIRECTORY)
 		print('Reference Genome is downloaded but needs to be prepared -- beginning process.')
-		prepref()
+		prepref(logfile)
 	else:
 		print('Reference Genome is already present in server and prepared!')
 	return True
 
 print('Beginning implementation.')
+logfile = open('logfile.txt', 'w')
 # step 0: create, source openstack openrc file to access chameleon cloud --> needs to be completed
 # step 1: check file_present function until it returns true
 while not osf.filepresent(CONTAINER, TO_BE_PROCESSED, "fastq"):
@@ -77,6 +78,12 @@ else:
 	print('There are already files ready for processing!')
 # step 4: run chosen pipeline on files
 # this requires the reference genome and snp_databases to be downloaded / prepared
-if checkref():
+if checkref(logfile):
 	print('The database and reference genome were downloaded and prepared.')
+# now to run the chosen pipeline --> currently just running sample.json in json folder
+chdir('/home/cc/data_processing/to_be_processed')
+if runp.runjsoncommands(JSON_DIRECTORY + argv[1], logfile):
+	print('Running %s was successful!' %argv[1])
 
+# once the process is done, close logfile
+logfile.close()
