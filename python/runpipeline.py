de18188a043a19720ba5054c42c17f49c602a3c9
Andrew Boles <drew.boles88@gmail.com>
2016-11-02 21:56:45 -0500

    Update runpipeline.py

--- python/runpipeline.py
+++ python/runpipeline.py
@@ -1,26 +1,5 @@
-# this script retrieves the fastq files from Swift, renames file and then runs the pipeline from json script
 import os, sys, re, json, time
 import collections
-
-def retrieveFile():
-	# first need to grab fastq file names from text file
-	filenames = []
-	# first I need to use os.environ to set two environmental variables for the bash script to accept --> in folder to_be_processed 
-	os.environ["input1"] = filenames[0]
-	os.environ["input2"] = filenames[1]
-	os.system('./download.sh') #need to make sure that it is executable!!!
-	os.system('cp ' + filenames[0] + ' input_R1.fastq')
-	os.system('cp ' + filenames[1] + ' input_R2.fastq')
-	return
-
-def renameFileName():
-	with open('filename.txt') as f:
-		name = f.readline().rstrip()
-	name = re.sub('_.*','',name)
-	os.system('mv exomecapstp8.bam ' + name+'_final.bam')
-	os.environ["output1"] = name + '_final.bam'
-	os.system('./upload.sh')
-	return
 
 def runjsoncommands(filename):
 	# Open the config file containing the commands, load into ordered dictionary
